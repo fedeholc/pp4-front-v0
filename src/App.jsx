416b93a5192f79d8d33d929e454ca40977bd871a
Federico Holc <federicoholc@gmail.com>
2025-05-10 19:09:37 -0300

    feat: integrate Radix UI themes and set up basic routing
    - Added @radix-ui/themes dependency to package.json
    - Updated App component to include Radix UI Theme and routing setup
    - Created Home component with a simple layout using Radix UI Button, Flex, and Text

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "@radix-ui/themes/styles.css";
+import { Button, Flex, Theme, Text } from "@radix-ui/themes";
 
 function App() {
-  return <>Hola</>;
+  return (
+    <>
+      <Theme accentColor="grass" grayColor="gray" radius="full">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </Theme>
+    </>
+  );
 }
 
 export default App;
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Flex direction="column" gap="2" width={"100%"} align="center">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Let's go</Button>
+      </Flex>
+    </div>
+  );
+}
