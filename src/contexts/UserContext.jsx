12c1cfb974467b6feae432fc07df6ceb6ebbec03
Federico Holc <federicoholc@gmail.com>
2025-05-17 16:00:23 -0300

    feat: update UserContext and UserProvider to use UsuarioCompleto type for improved user structure

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,3 +1,6 @@
 import { createContext } from "react";
 
+/** @typedef {import("../../types").UsuarioCompleto } UsuarioCompleto */
+/** @typedef {{ user: UsuarioCompleto | null, token: string | null,  setUser: React.Dispatch<React.SetStateAction<UsuarioCompleto | null>>, setToken: React.Dispatch<React.SetStateAction<string | null>>, logout: () => void }} UserContextType */
+/** @type {React.Context<UserContextType | null>} */
 export const UserContext = createContext(null);
